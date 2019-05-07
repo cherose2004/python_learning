1568844d5227847dda7a1fc9f887440fe696f02b
Amery_J <820293142@qq.com>
2019-05-07 12:39:53 +0800

    day28

---
+++ day28/4.osi七层模型.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# osi七层模型
+# 应用层
+    # 表示层
+    # 会话层
+    # 传输层
+    # 网络层
+    # 数据链路层
+    # 物理层
+
+# osi五层协议     协议                  物理设备
+    # 应用层      http https ftp smtp
+    #             python代码  hello
+    # 传输层      tcp/udp协议 端口      四层交换机、四层路由器
+    # 网络层      ipv4/ipv6协议         路由器、三层交换机
+    # 数据链路层  mac地址  arp协议      网卡 、交换机
+    # 物理层
