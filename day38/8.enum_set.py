938339226d78f0b2a123939e4b0944ced4fde862
Amery_J <820293142@qq.com>
2019-05-21 12:59:56 +0800

    day38

---
+++ day38/8.enum_set.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# create table t12(
+# name char(12),
+# gender ENUM('male','female'),
+# hobby set('抽烟','喝酒','烫头','洗脚')
+# );
+
+# insert into t12 values('alex','不详','抽烟,喝酒,洗脚,洗脚,按摩');
