7fe34308109ee0a527299c756b5933dea34baade
maple-shaw <2399669300@qq.com>
2019-07-03 18:18:12 +0800

    crm项目创建 实现登录注册功能

---
+++ Aida_crm/crm/forms.py
@@ -0,0 +1,40 @@
+from crm import models
+from django import forms
+from django.core.exceptions import ValidationError
+import hashlib
+
+class RegForm(forms.ModelForm):
+    password = forms.CharField(min_length=6,
+                               widget=forms.PasswordInput(attrs={'placeholder': '您的密码', 'autocomplete': 'off'}))
+    re_password = forms.CharField(min_length=6,
+                                  widget=forms.PasswordInput(attrs={'placeholder': '您的确认密码', 'autocomplete': 'off'}))
+
+    class Meta:
+        model = models.UserProfile
+        fields = '__all__'  # ['username']
+        exclude = ['is_active']
+        widgets = {
+            'username': forms.EmailInput(attrs={'placeholder': '您的用户名', 'autocomplete': 'off',}),
+            # 'password':forms.PasswordInput(attrs={'placeholder':'您的密码','autocomplete':'off'}),
+            'mobile': forms.TextInput(attrs={'placeholder': '您的手机号', 'autocomplete': 'off'}),
+            'name': forms.TextInput(attrs={'placeholder': '您的真实姓名', 'autocomplete': 'off'})
+        }
+        error_messages = {
+            'username': {
+                'required': '必填',
+                'invalid': '邮箱格式不正确'
+            }
+        }
+
+    def clean(self):
+        password = self.cleaned_data.get('password')
+        re_password = self.cleaned_data.get('re_password')
+        if password == re_password:
+            # 对密码进行加密
+            md5 = hashlib.md5()
+            md5.update(password.encode('utf-8'))
+            self.cleaned_data['password'] = md5.hexdigest()
+            return self.cleaned_data
+        else:
+            self.add_error('re_password', '两次密码不一致')
+            raise ValidationError('两次密码不一致!!')
---
+++ Aida_crm/crm/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect, reverse
+from crm import models
+import hashlib
+from crm.forms import RegForm
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        md5 = hashlib.md5()
+        md5.update(password.encode('utf-8'))
+
+        obj = models.UserProfile.objects.filter(username=username, password=md5.hexdigest(), is_active=True).first()
+        if obj:
+            return redirect(reverse('index'))
+            # return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': '用户名或密码错误'})
+    return render(request, 'login.html')
+
+
+def reg(request):
+    form_obj = RegForm()
+    if request.method == 'POST':
+        form_obj = RegForm(request.POST)
+        # 对提交的数据进行校验
+        if form_obj.is_valid():
+            # 校验成功  把数据插入数据中
+            # models.UserProfile.objects.create(**form_obj.cleaned_data)
+            form_obj.save()
+            return redirect(reverse('login'))
+        print(form_obj.errors)
+    return render(request, 'reg.html', {'form_obj': form_obj})
+
+
+def index(request):
+    return render(request, 'index.html')
