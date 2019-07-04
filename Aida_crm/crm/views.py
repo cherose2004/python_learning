dbb6f6c52f3001a1311890cacc022cf1478203ff
maple-shaw <2399669300@qq.com>
2019-07-04 16:17:55 +0800

    分页页封装成类

--- Aida_crm/crm/views.py
+++ Aida_crm/crm/views.py
@@ -46,72 +46,9 @@ def customer_list(request):
 
 users = [{'name': 'alex-{}'.format(i), 'pwd': 'alexdsb'} for i in range(1, 453)]
 
+from utils.pagination import Pagination
+
 
 def user_list(request):
-    try:
-        page = int(request.GET.get('page', 1))
-        if page <= 0:
-            page = 1
-    except Exception:
-        page = 1
-
-    # 总的数据量
-    all_count = len(users)
-    # 每页显示的数据量  10
-    per_num = 10
-    # 总的页码数
-    total_num, more = divmod(all_count, per_num)
-    if more:
-        total_num += 1
-    # 最大显示的页码数
-    max_show = 11
-    half_show = max_show // 2
-
-    if total_num <= max_show:
-        page_start = 1
-        page_end = total_num
-    else:
-        if page - half_show<=0:
-            # 页码的起始值
-            page_start = 1
-            # 页码的终止值
-            page_end = max_show
-        elif page + half_show > total_num:
-            page_end = total_num
-            page_start = total_num - max_show + 1
-
-        else:
-            # 页码的起始值
-            page_start = page - half_show
-            # 页码的终止值
-            page_end = page + half_show
-    #   1  0  10
-    #   2  10  20
-    start = (page - 1) * per_num
-    end = page * per_num
-
-    li_list = []
-
-    if page == 1:
-        li_list.append(
-            '<li class="disabled"><a aria-label="Previous"> <span aria-hidden="true">&laquo;</span></a></li>')
-    else:
-        li_list.append('<li><a href="?page={}" aria-label="Previous"> <span aria-hidden="true">&laquo;</span></a></li>'.format(page-1))
-
-    for i in range(page_start,page_end+1):
-        if i == page:
-            li_list.append('<li class="active"><a href="?page={}">{}</a></li>'.format(i, i))
-        else:
-            li_list.append('<li><a href="?page={}">{}</a></li>'.format(i,i))
-
-    if page == total_num:
-        li_list.append(
-            '<li class="disabled"><a aria-label="Next"> <span aria-hidden="true">&raquo;</span></a></li>')
-    else:
-        li_list.append('<li><a href="?page={}" aria-label="Next"> <span aria-hidden="true">&raquo;</span></a></li>'.format(page+1))
-
-
-
-    page_html = ''.join(li_list)
-
-    return render(request, 'user_list.html', {'users': users[start:end],'page_html':page_html })
+    page = Pagination(request.GET.get('page', 1), len(users),20,15)
+    return render(request, 'user_list.html', {'users': users[page.start:page.end], 'page_html': page.page_html})
